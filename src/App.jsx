abc27729acc3560ca8c9b99cd2829c46fc10ee75
Dima Voropaev <dima6343210@yandex.ru>
2023-12-29 16:57:22 +0300

    removed unused App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-
 import { LandCard } from './components/LandCard/LandCard'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <LandCard />
